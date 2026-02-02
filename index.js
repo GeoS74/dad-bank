4c1cbb433dff1338877d68077f732e06b9eecbaa
geos <gsirotkin@yandex.ru>
2026-02-02 14:05:56 +0500

    flow

--- index.js
+++ index.js
@@ -3,25 +3,22 @@ const logger = require('./libs/logger');
 const config = require('./config');
 const {isUserAllowed, user, isDady, checkNameUser} = require('./middleware/user');
 const keyboard = require('./middleware/keyboard');
-const {deposit, withdraw, stateCheck} = require('./middleware/action');
+const {deposit, withdraw, stateCheck, stateMyCheck} = require('./middleware/action');
 
 const bot = new Telegraf(config.bot.token);
 
 bot.use(isUserAllowed, user);
 bot.start(keyboard);
 
-bot.hears('Мой счёт', ctx => {
-    ctx.reply('Привет Алиса! У тебя на счёте 2523 р.');
-});
-
 bot.hears(/^пополнить\s+\S+\s+\d+$/i, isDady, checkNameUser, deposit);
 bot.hears(/^снять\s+\S+\s+\d+$/i, isDady, checkNameUser, withdraw);
 bot.hears(/^депозит\s+\S+$/i, isDady, checkNameUser, stateCheck);
 
+bot.hears('Мой счёт', stateMyCheck);
 bot.hears('Условия вклада', ctx => {
     ctx.reply(`Привет, ${ctx.user.name}! Вот условия вклада в Папа-Банке:\n
         1) процентная ставка по вкладу составляет 10% в месяц;\n
-        2) проценты по вкладу начисляются на остаток по счёту до 15-го числа;
+        2) проценты по вкладу начисляются на остаток по счёту до 15-го числа;\n
         3) пополнять и снимать деньги можно в любой момент;\n
         `);
 });
--- middleware/action.js
+++ middleware/action.js
@@ -1,24 +1,59 @@
 const db = require('../libs/db');
-const { users } = require('../middleware/user');
 
 module.exports.deposit = async (ctx) => {
-  const [action, name, amount] = ctx.message.text.split(' ');
-  console.log(`${action} ${name} ${amount}`);
-
-  // if(!users.find(u => u.name === name)) {
-  //   return ctx.reply(`Не верно указано имя клиента банка!`);
-  // }
-  ctx.reply(`Пополнение счёта!`);
+  const [action, username, price] = ctx.message.text.split(' ');
+  await _deposit(action, username, price);
+  ctx.reply(`Деньги внесены на счёт!`);
 }
 
 module.exports.withdraw = async (ctx) => {
-  const [action, name, amount] = ctx.message.text.split(' ');
-  console.log(`${action} ${name} ${amount}`);
+  const [action, username, price] = ctx.message.text.split(' ');
+  await _deposit(action, username, -price);
   ctx.reply(`Снятие денег!`);
 }
 
 module.exports.stateCheck = async (ctx) => {
-  const [action, name] = ctx.message.text.split(' ');
-  console.log(`${action} ${name}`);
-  ctx.reply(`Состояние счёта`);
+  const [action, username] = ctx.message.text.split(' ');
+
+  let state = await _stateCheck(username);
+
+  if(!state) {
+    return ctx.reply(`Состояние счёта ${username}\nдоступный остаток: 0 р.`);
+  }
+
+  state = state.toLocaleString('ru-RU', {
+    maximumFractionDigits: 0
+  });
+
+  ctx.reply(`Состояние счёта ${username}\n
+      доступный остаток: ${state} р.
+    `);
+}
+
+async function _deposit(username, action, price) {
+  return db.query(`INSERT INTO transaction
+    (action, username, price)
+    VALUES ($1, $2, $3)
+    RETURNING *
+    `, [username, action, price]);
+}
+
+async function _stateCheck(username) {
+  return db.query(`SELECT sum(price) as sum FROM transaction WHERE username=$1`,
+    [username])
+    .then((res) => res.rows[0]?.sum);
+}
+
+module.exports.stateMyCheck = async (ctx) => {
+  let state = await _stateCheck(ctx.user.name);
+
+  if(!state) {
+    return ctx.reply(`Привет, ${ctx.user.name}!\nдоступный остаток по счёту: 0 р.`);
+  }
+
+  state = state.toLocaleString('ru-RU', {
+    maximumFractionDigits: 0
+  });
+
+  ctx.reply(`Привет ${ctx.user.name}!\nдоступный остаток по счёту: ${state} р.`);
 }
